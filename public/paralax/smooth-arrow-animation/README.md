00c3701eb9cb49072b65105bbf0b61ed0edc002f
Alexey Grevtsov <grevold@yandex.ru>
2023-08-14 22:28:22 +0300

    Добавил страницу с реквизитами

--- public/paralax/smooth-arrow-animation/README.md
+++
@@ -1,4 +0,0 @@
-# Smooth arrow animation
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/vlt_dev/pen/NWMNzpE](https://codepen.io/vlt_dev/pen/NWMNzpE).
-
--- src/pages/RequisitesPage/RequisitesPage.tsx
+++ src/pages/RequisitesPage/RequisitesPage.tsx
@@ -1,65 +1,55 @@
+import { Link } from "react-router-dom";
 import s from "./RequisitesPage.module.css";
 
 export function RequisitesPage() {
-  function playKick() {
-    let audio = new Audio(
-      `${process.env.PUBLIC_URL}/Samples/Drums/Kick_02.mp3`
-    );
-    audio.play();
-  }
-  function playHat() {
-    let audio = new Audio(`${process.env.PUBLIC_URL}/Samples/Drums/Hat_01.mp3`);
-    audio.play();
-  }
-  function playClap() {
-    let audio = new Audio(
-      `${process.env.PUBLIC_URL}/Samples/Drums/Clap_06.mp3`
-    );
-    audio.play();
-  }
-  function playBass() {
-    let audio = new Audio(
-      `${process.env.PUBLIC_URL}/Samples/Drums/Bass_01.mp3`
-    );
-    audio.play();
-  }
-
-  if (window.screen.width > 1000) {
-    return (
-      <div className={s.root}>
-        <div className={s.playground}>
-          <button className={s.sound} onMouseDown={playKick}>
-            Kick
-          </button>
-          <button className={s.sound} onMouseDown={playHat}>
-            Hat
-          </button>
-          <button className={s.sound} onMouseDown={playClap}>
-            Clap
-          </button>
-          <button className={s.sound} onMouseDown={playBass}>
-            Bass
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className={s.root}>
-      <div className={s.playground}>
-        <button className={s.sound} onTouchStart={playKick}>
-          Kick
-        </button>
-        <button className={s.sound} onTouchStart={playHat}>
-          Hat
-        </button>
-        <button className={s.sound} onTouchStart={playClap}>
-          Clap
-        </button>
-        <button className={s.sound} onTouchStart={playBass}>
-          Bass
-        </button>
+      <div className={s.document}>
+        <h2 className={s.header}>Реквизиты</h2>
+        <Link
+          to={
+            "https://drive.google.com/file/d/1nltVXtGX-Vwz4E1v4gfxxmvBd-qeTWjv/view?usp=drive_link"
+          }
+          target="_blank"
+        >
+          <div className={s.container}>
+            <img
+              className={s.prev}
+              src={`${process.env.PUBLIC_URL}/requisites/prev_2.png`}
+            />
+          </div>
+        </Link>
+        <a
+          className={s.button}
+          href={`${process.env.PUBLIC_URL}/requisites/Requisites.pdf`}
+          download
+        >
+          Скачать
+        </a>
+      </div>
+
+      <div className={s.document}>
+        <h2 className={s.header}>СРО</h2>
+        <Link
+          to={
+            "https://drive.google.com/file/d/1XBpZwpSvTJg3orz3TSZQKa8APz8b1WGk/view?usp=drive_link"
+          }
+          target="_blank"
+        >
+          <div className={s.container}>
+            <img
+              className={s.prev}
+              src={`${process.env.PUBLIC_URL}/requisites/prev_1.png`}
+            />
+          </div>
+        </Link>
+        <a
+          className={s.button}
+          href={`${process.env.PUBLIC_URL}/requisites/SRO.pdf`}
+          download
+        >
+          Скачать
+        </a>
       </div>
     </div>
   );
